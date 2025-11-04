e125b3998193c7d6fcf3f3ad5986127357604820
FilipeGS1 <filipegr094@gmail.com>
2025-11-03 23:20:15 -0300

    lados

--- Projeto Top Down/Assets/Scripts/Player.cs
+++ Projeto Top Down/Assets/Scripts/Player.cs
@@ -18,24 +18,42 @@ public class Player : Personagem
 
     void Update()
     {
+        andando =  false;
+
+        if (arma.rotation.eulerAngles.z > -90 && arma.rotation.eulerAngles.z < 90)
+        {
+            spriteRenderer.flipX = false;
+        }
+        
+        if (arma.rotation.eulerAngles.z > 90 && arma.rotation.eulerAngles.z < 270)
+        {
+            spriteRenderer.flipX = true;
+        }
+        
         if (Input.GetKey(KeyCode.W))
         {
             gameObject.transform.position += new Vector3(0, getVelocidade() * Time.deltaTime, 0);
+            andando =  true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             gameObject.transform.position -= new Vector3(0, getVelocidade() * Time.deltaTime, 0);
+            andando =  true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position += new Vector3(getVelocidade() * Time.deltaTime, 0, 0);
+            andando =  true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position -= new Vector3(getVelocidade() * Time.deltaTime, 0, 0);
+            andando =  true;
         }
+        
+        animator.SetBool("Andando", andando);
     }
 }
